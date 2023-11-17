1f08e2cfe457a745acdbc98ad7d4b1dc71fc3ad2
Ishaan Masil <61901829+StarkOdinson612@users.noreply.github.com>
2023-11-17 07:22:54 -0600

    Added single page input screen

--- src/GUI/UIParent.py
+++ src/GUI/UIParent.py
@@ -32,14 +32,47 @@ class UIParent(customtkinter.CTkFrame):
         self.t_entry.grid(row=1, column=1, stick="nsew", padx=5, pady=5)
 
 
-class InputsApp(customtkinter.CTk):
+class App(customtkinter.CTk):
     def __init__(self):
+        super().__init__()
+
+        self.title("Not Discord")
+        self.geometry("1600x900")
+        self.minsize(750, 400)
+        # ip_dialog = customtkinter.CTkInputDialog(text="Enter the IP to connect to: ",
+        #                                          title="IP Address")
+        # self.IP = ip_dialog.get_input()
+        #
+        # port_dialog = customtkinter.CTkInputDialog(text="Enter the port: ",
+        #                                            title="Port")
+        # self.PORT = int(port_dialog.get_input())
+        # name_dialog = customtkinter.CTkInputDialog(text="Enter username: ",
+        #                                            title="Client Username")
+        # self.NAME = name_dialog.get_input()
+
+        user_inp = InputsApp(self)
+        self.INP_U = user_inp.get_data()
+        self.IP = self.INP_U[0]
+        self.PORT = self.INP_U[1]
+        self.NAME = self.INP_U[2]
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.ui_parent = UIParent(master=self, port=self.PORT, ip=self.IP, name=self.NAME)
+        self.ui_parent.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+    def setInp(self, inp: (str, int, str)):
+        self.INP_U = inp
+
+
+class InputsApp(customtkinter.CTkToplevel):
+    def __init__(self, master_a: App):
         super().__init__()
 
         self.title("Connection Window")
         self.geometry("450x300")
         self.minsize(200, 100)
         self.protocol("WM_DELETE_WINDOW", self._on_closing)
+        self.master_a = master_a
 
         self.user_inp: (str, int, str) = None
 
@@ -64,8 +97,14 @@ class InputsApp(customtkinter.CTk):
         self.sub_but = customtkinter.CTkButton(self, text="Connect", command=self._button_submit)
         self.sub_but.grid(row=1, column=0)
 
+        self.lift()
+        self.attributes("-topmost", True)
+
     def _button_submit(self):
+        print("sub")
+        self.master_a.setInp((self.ip.get(), int(self.port.get()), self.name.get()))
         self.user_inp = (self.ip.get(), int(self.port.get()), self.name.get())
+        print("bob")
         self.grab_release()
         self.destroy()
 
@@ -76,35 +115,3 @@ class InputsApp(customtkinter.CTk):
     def _on_closing(self):
         self.grab_release()
         self.destroy()
-
-
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Not Discord")
-        self.geometry("1600x900")
-        self.minsize(750, 400)
-        ip_dialog = customtkinter.CTkInputDialog(text="Enter the IP to connect to: ",
-                                                 title="IP Address")
-        self.IP = ip_dialog.get_input()
-
-        port_dialog = customtkinter.CTkInputDialog(text="Enter the port: ",
-                                                   title="Port")
-        self.PORT = int(port_dialog.get_input())
-        name_dialog = customtkinter.CTkInputDialog(text="Enter username: ",
-                                                   title="Client Username")
-        self.NAME = name_dialog.get_input()
-        # user_inp = InputsApp()
-        # user_inp.mainloop()
-        # print("adfdfa")
-        # self.INP_U = user_inp.get_data()
-        # self.IP = self.INP_U[0]
-        # self.PORT = self.INP_U[1]
-        # self.NAME = self.INP_U[2]
-        # self.IP = "localhost"
-        # self.PORT = 50123
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.ui_parent = UIParent(master=self, port=self.PORT, ip=self.IP, name=self.NAME)
-        self.ui_parent.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
